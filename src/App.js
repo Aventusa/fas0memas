8220d847d8cf4eded2a0f2fe80403a2368eb3138
Aventusa <Aventusa@yandex.ru>
2020-12-14 18:17:50 +0300

    add new component and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 import Header from './components/header/Header';
 import Generator from './components/pages/generator/Generator';
 import Footer from './components/footer/Footer';
+import About from './components/pages/about/About';
 
 function App() {
 
     return (
         <div className="App">
-            <Header/>
-            <Generator />
-            <Footer />
+            <Router>
+                <Header/>
+                <Switch>
+                    <Route exact path='/'>
+                        <Generator/>
+                    </Route>
+                    <Route exact path='/about'>
+                        <About/>
+                    </Route>
+                </Switch>
+                <Footer/>
+            </Router>
         </div>
     )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Header.css'
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
         <header className='header'>
+            <Link to="/">fas0memas</Link>
+            <Link to='/about' className='icon-about'>
 
+            </Link>
         </header>
     )
 }
