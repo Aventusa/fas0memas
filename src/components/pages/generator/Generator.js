e08ed03288dde1732b0bff7896ce6f5783e68d05
Aventusa <Aventusa@yandex.ru>
2021-01-07 12:50:43 +0300

    form redesign and server request

--- src/components/pages/generator/Generator.js
+++ src/components/pages/generator/Generator.js
@@ -1,19 +1,17 @@
 import React, {useEffect, useRef, useState} from 'react'
 import './Generator.css'
-// import Preloader from '../../preloader/Preloader';
 import Result from '../../result/Result';
 import Preloader from '../../preloader/Preloader';
 
 function Generator() {
     // eslint-disable-next-line no-unused-vars
     const [loading, setLoading] = useState(false)
+    const [fileData, setFileData] = useState(null)
     const [urlData, setUrlData] = useState({src: '', textTop: '', textBottom: ''})
-
     useEffect(() => {
         setLoading(true)
         fetch(urlFormation(urlData))
             .then(response => {
-                console.log(response)
                 response ? setLoading(false) : null
             })
             .catch(error => console.log(error))
@@ -30,31 +28,68 @@ function Generator() {
 
     function submitForm(e) {
         e.preventDefault()
+
+        if (fileData !== null) {
+            let formData = new FormData()
+            formData.append('file', fileData)
+            fetch(`${window.location.href}/create.php`, {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => {
+                    console.log('RESPONSE =>>', response)
+                    setUrlData(
+                        {
+                            src: window.location.href + `img/${fileData.name}`,
+                            textTop: textTop.current.value,
+                            textBottom: textBottom.current.value,
+                        }
+                    )
+                })
+                .catch(error => console.log('Ошибка! ---', error))
+            return
+        }
+
         setUrlData(
             {
                 src: src.current.value,
                 textTop: textTop.current.value,
                 textBottom: textBottom.current.value,
-
             }
         )
     }
 
-    function onResultLoad() {
+    function onResultLoad(file) {
+        setFileData(file)
         setLoading(false)
     }
+
 
     return (
         <div className='generator'>
             <div className="generator__wrapper">
-                {loading ? <Preloader/> : <Result load={onResultLoad} src={urlFormation(urlData)}/>}
+                {
+                    loading
+                        ? <Preloader/>
+                        : <Result
+                            load={onResultLoad}
+                            src={urlData.src !== '' ? urlFormation(urlData) : null}
+                        />
+                }
+
                 <form className='generator__form' onSubmit={(e) => submitForm(e)}>
-                    <input name='src' ref={src} className='generator__input' type="text"
-                           placeholder='Введите url изображения'/>
-                    <input name='textTop' ref={textTop} className='generator__input' type="text"
-                           placeholder='Введите вверхний текст'/>
-                    <input name='textBottom' ref={textBottom} className='generator__input' type="text"
-                           placeholder='Введите нижний текст'/>
+                    <div className="input__wrapper">
+                        <input name='src' ref={src} className='generator__input' type="text"
+                               placeholder='Введите url изображения'/>
+                    </div>
+                    <div className="input__wrapper">
+                        <input name='textTop' ref={textTop} className='generator__input' type="text"
+                               placeholder='Введите вверхний текст'/>
+                    </div>
+                    <div className="input__wrapper">
+                        <input name='textBottom' ref={textBottom} className='generator__input' type="text"
+                               placeholder='Введите нижний текст'/>
+                    </div>
                     <button
                         type='submit'
                         className='generator__btn'
--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import './Result.css'
 
 function Result(props) {
+    const [src, setSrc] = useState(props.src)
+    const [file, setFile] = useState(null)
+    function dragStartHandler(e) {
+        e.preventDefault()
+    }
+
+    function dragLeaveHandler(e) {
+        e.preventDefault()
+    }
+
+    function onDropHandler(e) {
+        e.preventDefault()
+
+        let files = [...e.dataTransfer.files]
+        setFile(files[0])
+        files.forEach(file => {
+            file.type.includes('image') ? previewFile(file) : null
+        })
+    }
+
+    function previewFile(file) {
+        let reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = function () {
+            setSrc(reader.result)
+        }
+    }
     return (
-        <div>
-            <img className='mem-img' src={props.src} onLoad={props.load} alt=""/>
+
+        <div className='result'>
+
+            <div className={src == null ? 'drop-area' : 'drop-area active'}
+                 onDragStart={e => dragStartHandler(e)}
+                 onDragLeave={e => dragLeaveHandler(e)}
+                 onDragOver={e => dragStartHandler(e)}
+                 onDrop={e => onDropHandler(e)}
+            />
+            <img className='mem-img' src={src} onLoad={file !== null ? props.load(file) : props.load} alt=""/>
         </div>
     )
 }
